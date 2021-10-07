4619b61ae42eb9070c6f8c28d48d47e1bba7cb66
Illia Likhoshva <ilyaliko64@gmail.com>
2021-10-07 19:23:29 +0300

    Update index.ts

--- src/commands/stop-sandbox/index.ts
+++ src/commands/stop-sandbox/index.ts
@@ -4,7 +4,7 @@ import { em, debug, getCWD } from "../../console";
 import { ContractsBundle } from '../../modules/bundle';
 import { startFlextesa } from "../../modules/flextesa";
 
-export const addStartSandboxCommand = (program: Command, debugHook: (cmd: Command) => void) => {
+export const addStopSandboxCommand = (program: Command, debugHook: (cmd: Command) => void) => {
   program
     .command('stop-sandbox')
     .description('Stops a local Flextesa Tezos test network.')
