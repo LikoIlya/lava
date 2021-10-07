ffa01f8f957582ccd16d42e9b442a056a05d26fd
Illia Likhoshva <ilyaliko64@gmail.com>
2021-10-07 19:21:57 +0300

    Add stop-sandbox command

---
+++ src/commands/stop-sandbox/index.ts
@@ -0,0 +1,21 @@
+import { Command } from 'commander';
+
+import { em, debug, getCWD } from "../../console";
+import { ContractsBundle } from '../../modules/bundle';
+import { startFlextesa } from "../../modules/flextesa";
+
+export const addStartSandboxCommand = (program: Command, debugHook: (cmd: Command) => void) => {
+  program
+    .command('stop-sandbox')
+    .description('Stops a local Flextesa Tezos test network.')
+    .action(() => {
+      stopSandbox();
+    })
+    .hook('preAction', debugHook);
+}
+
+// Full start-sandbox command controller
+export const stopSandbox = async (readyCallback?: () => void) => {
+  em(`Stopping Tezos sandbox...\n`);
+  stopFlextesa(readyCallback);
+};
